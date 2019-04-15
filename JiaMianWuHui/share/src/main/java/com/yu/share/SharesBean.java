5be58c40305482caf9a7d35d07d23de597333a39
yudenghao <denghao1019@163.com>
2019-04-15 18:04:57 +0800

    add:修改分享

---
+++ JiaMianWuHui/share/src/main/java/com/yu/share/SharesBean.java
@@ -0,0 +1,15 @@
+package com.yu.share;
+
+import java.io.Serializable;
+
+/**
+ * @author yudneghao
+ * @date 2019/4/15
+ */
+public class SharesBean implements Serializable {
+  public String type;
+  public String title;
+  public String content;
+  public String targetUrl;
+  public String iconUrl;
+}
