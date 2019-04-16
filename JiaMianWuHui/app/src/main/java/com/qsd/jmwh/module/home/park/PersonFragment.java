09efcc17f655662ceecb3476fee2bdedf583812a
mks <keshuang.ma@easwift.cn>
2019-04-17 00:13:03 +0800

    commit 首页person列表

--- JiaMianWuHui/app/src/main/java/com/qsd/jmwh/module/home/park/PersonFragment.java
+++ JiaMianWuHui/app/src/main/java/com/qsd/jmwh/module/home/park/PersonFragment.java
@@ -2,11 +2,18 @@ package com.qsd.jmwh.module.home.park;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.qsd.jmwh.R;
 import com.qsd.jmwh.base.BaseFragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PersonFragment extends BaseFragment {
+    private List<String> list = new ArrayList<>();
+
     @Override
     protected int getContentViewId() {
         return R.layout.person_fragment_layout;
@@ -19,6 +26,15 @@ public class PersonFragment extends BaseFragment {
 
     @Override
     protected void loadData() {
+        for (int i = 0; i < 10; i++) {
+            list.add("");
+        }
+        initView();
+    }
+
+    private void initView() {
+        RecyclerView rv_person = bindView(R.id.rv_person);
+        rv_person.setLayoutManager(new LinearLayoutManager(getActivity()));
 
     }
 }
