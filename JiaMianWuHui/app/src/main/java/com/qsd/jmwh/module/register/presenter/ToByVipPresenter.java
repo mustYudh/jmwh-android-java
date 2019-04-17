b3f831f3df0fadcdb72aec3bc7ccfc2472c85a48
yudenghao <denghao.yu@yuntu-inc.com>
2019-04-17 23:58:40 +0800

    add：增加Vip代码

---
+++ JiaMianWuHui/app/src/main/java/com/qsd/jmwh/module/register/presenter/ToByVipPresenter.java
@@ -0,0 +1,10 @@
+package com.qsd.jmwh.module.register.presenter;
+
+import com.yu.common.framework.BaseViewPresenter;
+
+public class ToByVipPresenter extends BaseViewPresenter<ToByVipViewer> {
+
+    public ToByVipPresenter(ToByVipViewer viewer) {
+        super(viewer);
+    }
+}
