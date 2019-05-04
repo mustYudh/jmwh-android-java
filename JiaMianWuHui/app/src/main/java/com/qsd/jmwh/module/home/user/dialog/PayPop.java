e84b5d55c4556943343567eb3f0acc8a71d5eb93
yudenghao <denghao.yu@yuntu-inc.com>
2019-05-04 09:28:37 +0800

    add:增加弹窗

---
+++ JiaMianWuHui/app/src/main/java/com/qsd/jmwh/module/home/user/dialog/PayPop.java
@@ -0,0 +1,46 @@
+package com.qsd.jmwh.module.home.user.dialog;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.qsd.jmwh.R;
+import com.yu.common.windown.BasePopupWindow;
+
+public class PayPop extends BasePopupWindow {
+
+    private final TextView ok;
+    private final TextView cancel;
+
+
+    public PayPop(Context context) {
+        super(context, LayoutInflater.from(context).inflate(R.layout.pay_pop_layouy, null),
+                RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+        ok = (TextView) findViewById(R.id.ok);
+        cancel = (TextView) findViewById(R.id.cancel);
+    }
+
+
+    public PayPop setPositiveButton(View.OnClickListener listener) {
+        ok.setOnClickListener(listener);
+        return this;
+    }
+
+    public PayPop setSingleButton(View.OnClickListener listener) {
+        cancel.setVisibility(View.GONE);
+        ok.setVisibility(View.VISIBLE);
+        return this;
+    }
+
+    @Override
+    protected View getBackgroundShadow() {
+        return null;
+    }
+
+    @Override
+    protected View getContainer() {
+        return null;
+    }
+}
---
+++ JiaMianWuHui/app/src/main/java/com/qsd/jmwh/module/home/user/dialog/SelePayTypePop.java
@@ -0,0 +1,26 @@
+package com.qsd.jmwh.module.home.user.dialog;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.qsd.jmwh.R;
+import com.yu.common.windown.BasePopupWindow;
+
+public class SelePayTypePop extends BasePopupWindow {
+    public SelePayTypePop(Context context, View contentView, int width, int height) {
+        super(context, LayoutInflater.from(context).inflate(R.layout.sele_pay_type_pop_layout, null),
+                RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+    }
+
+    @Override
+    protected View getBackgroundShadow() {
+        return null;
+    }
+
+    @Override
+    protected View getContainer() {
+        return null;
+    }
+}
