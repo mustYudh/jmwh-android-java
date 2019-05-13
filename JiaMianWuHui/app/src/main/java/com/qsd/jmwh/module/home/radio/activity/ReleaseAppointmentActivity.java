0bfd4d09ceb013d5d66a58545059539a848beab7
mks <keshuang.ma@easwift.cn>
2019-05-13 11:11:41 +0800

    Commit 约会电台 切换以及点击事件

--- JiaMianWuHui/app/src/main/java/com/qsd/jmwh/module/home/radio/activity/ReleaseAppointmentActivity.java
+++ JiaMianWuHui/app/src/main/java/com/qsd/jmwh/module/home/radio/activity/ReleaseAppointmentActivity.java
@@ -1,12 +1,36 @@
 package com.qsd.jmwh.module.home.radio.activity;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.qsd.jmwh.R;
 import com.qsd.jmwh.base.BaseBarActivity;
+import com.qsd.jmwh.view.CircleImageView;
+import com.qsd.jmwh.view.NoSlidingGridView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ch.ielse.view.SwitchView;
+import cn.finalteam.rxgalleryfinal.RxGalleryFinalApi;
+import cn.finalteam.rxgalleryfinal.rxbus.RxBusResultDisposable;
+import cn.finalteam.rxgalleryfinal.rxbus.event.ImageRadioResultEvent;
 
 public class ReleaseAppointmentActivity extends BaseBarActivity {
+
+    private NoSlidingGridView gv_photo;
+    private List<String> allLocationSelectedPicture = new ArrayList<>();
+    private GridAdapter adapter;
 
     @Override
     protected void setView(@Nullable Bundle savedInstanceState) {
@@ -15,6 +39,112 @@ public class ReleaseAppointmentActivity extends BaseBarActivity {
 
     @Override
     protected void loadData() {
-        setTitle("发布约会");
+        String activity_title = getIntent().getStringExtra("activity_title");
+        setTitle(activity_title);
+        LinearLayout ll_qiwang = bindView(R.id.ll_qiwang);
+        LinearLayout ll_city = bindView(R.id.ll_city);
+        LinearLayout ll_time = bindView(R.id.ll_time);
+        TextView tv_sure = bindView(R.id.tv_sure);
+        TextView tv_qiwang = bindView(R.id.tv_qiwang);
+        TextView tv_city = bindView(R.id.tv_city);
+        TextView tv_time = bindView(R.id.tv_time);
+        EditText et_buchong = bindView(R.id.et_buchong);
+        SwitchView sw_one = bindView(R.id.sw_one);
+        SwitchView sw_two = bindView(R.id.sw_two);
+
+
+        gv_photo = bindView(R.id.gv_photo);
+        adapter = new GridAdapter();
+        gv_photo.setAdapter(adapter);
+
+
+    }
+
+
+    private class GridAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            if (allLocationSelectedPicture.size() == 3) {
+                return allLocationSelectedPicture.size();
+            }
+            return allLocationSelectedPicture.size() + 1;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+
+        @Override
+        public View getView(final int position, View convertView, final ViewGroup parent) {
+            ViewHolder holder = null;
+            if (convertView == null) {
+                convertView = View.inflate(getActivity(), R.layout.gv_item, null);
+                holder = new ViewHolder();
+                holder.ivDemo = (ImageView) convertView.findViewById(R.id.iv_picture_item);
+                holder.ivDel = (CircleImageView) convertView.findViewById(R.id.iv_del);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+            if (position == allLocationSelectedPicture.size()) {
+                if (position == 3) {
+                    holder.ivDemo.setVisibility(View.GONE);
+                }
+
+                holder.ivDemo.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pic_add));
+                holder.ivDel.setVisibility(View.GONE);
+                holder.ivDemo.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        addPhoto();
+                    }
+                });
+            } else {
+                Glide.with(getActivity()).load(allLocationSelectedPicture.get(position) + "").into(holder.ivDemo);
+                holder.ivDel.setVisibility(View.VISIBLE);
+            }
+            holder.ivDel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    allLocationSelectedPicture.remove(position);
+                    if (adapter != null) {
+                        gv_photo.setAdapter(adapter);
+                    }
+                }
+            });
+            return convertView;
+        }
+    }
+
+
+    public class ViewHolder {
+        public ImageView ivDemo;
+        public CircleImageView ivDel;
+    }
+
+
+    private void addPhoto() {
+        RxGalleryFinalApi instance = RxGalleryFinalApi.getInstance(getActivity());
+        instance.openSingGalleryRadioImgDefault(
+                new RxBusResultDisposable<ImageRadioResultEvent>() {
+                    @Override
+                    protected void onEvent(ImageRadioResultEvent imageRadioResultEvent) {
+                        String imgUrl = imageRadioResultEvent.getResult().getOriginalPath();
+                        if (!TextUtils.isEmpty(imgUrl)) {
+                            allLocationSelectedPicture.add(imgUrl);
+                            if (adapter != null) {
+                                gv_photo.setAdapter(adapter);
+                            }
+                        }
+                    }
+                });
     }
 }
--- JiaMianWuHui/app/src/main/java/com/qsd/jmwh/module/home/radio/adapter/HomeRadioRvAdapter.java
+++ JiaMianWuHui/app/src/main/java/com/qsd/jmwh/module/home/radio/adapter/HomeRadioRvAdapter.java
@@ -1,15 +1,18 @@
 package com.qsd.jmwh.module.home.radio.adapter;
 
 import android.content.Context;
+import android.view.View;
 
 import com.bumptech.glide.Glide;
 import com.chad.library.adapter.base.BaseMultiItemQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.qsd.jmwh.R;
+import com.qsd.jmwh.data.UserProfile;
 import com.qsd.jmwh.module.home.radio.bean.LocalHomeRadioListBean;
 import com.qsd.jmwh.view.CircleImageView;
 import com.qsd.jmwh.view.NoSlidingGridView;
 import com.yu.common.ui.DelayClickImageView;
+import com.yu.common.ui.DelayClickTextView;
 
 import java.util.List;
 
@@ -52,9 +55,44 @@ public class HomeRadioRvAdapter extends BaseMultiItemQuickAdapter<LocalHomeRadio
                 }
                 break;
             case 2:
+                DelayClickImageView iv_like = helper.getView(R.id.iv_like);
+                DelayClickImageView iv_apply = helper.getView(R.id.iv_apply);
+                DelayClickTextView tv_like = helper.getView(R.id.tv_like);
+                DelayClickTextView tv_apply = helper.getView(R.id.tv_apply);
+                if (item.is_apply == 0) {
+                    iv_apply.setImageResource(R.drawable.apply);
+                } else {
+                    iv_apply.setImageResource(R.drawable.apply_select);
+                }
+                tv_apply.setText("报名(" + item.nApplyCount + ")");
+                if (item.is_like == 0) {
+                    iv_like.setImageResource(R.drawable.zan);
+                } else {
+                    iv_like.setImageResource(R.drawable.zan_select);
+                }
                 helper.setText(R.id.tv_comment, "评论(" + item.count_num + ")");
                 helper.setText(R.id.tv_like, "赞(" + item.like_count + ")");
+
+                helper.getView(R.id.ll_zan).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (onRadioItemClickListener != null) {
+                            onRadioItemClickListener.setOnRadioItemClick(iv_like, tv_like, helper.getLayoutPosition(), item.is_like, item.lDatingId + "", UserProfile.getInstance().getAppAccount() + "", item.lUserId + "");
+                        }
+                    }
+                });
                 break;
         }
+    }
+
+
+    public interface OnRadioItemClickListener {
+        void setOnRadioItemClick(DelayClickImageView iv_like, DelayClickTextView tv_like, int position, int is_like, String lDatingId, String lJoinerId, String lInitiatorId);
+    }
+
+    OnRadioItemClickListener onRadioItemClickListener;
+
+    public void setOnPersonItemClickListener(OnRadioItemClickListener onRadioItemClickListener) {
+        this.onRadioItemClickListener = onRadioItemClickListener;
     }
 }
