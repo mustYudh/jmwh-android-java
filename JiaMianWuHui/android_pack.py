e29d124e7f9fc79b5b9a3cce9677e560e335e5d0
yudenghao <denghao1019@163.com>
2019-05-15 18:06:31 +0800

    add:修改UI

--- JiaMianWuHui/android_pack.py
+++ JiaMianWuHui/android_pack.py
@@ -2,5 +2,3 @@
 import os
 os.system("./gradlew clean assembleJmwhRelease -PAPI_MODE=ONLINE");
 
-print '开始执行安装命令:adb install -r app/build/outputs/apk/jmwh/release/app-release.apk';
-os.system("adb install -r app/build/outputs/apk/jmwh/release/app-release.apk");
