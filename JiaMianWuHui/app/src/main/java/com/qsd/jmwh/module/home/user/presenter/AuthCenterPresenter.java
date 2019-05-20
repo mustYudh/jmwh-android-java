44b343ed3bad1e4eb28459449280530b21cbcc94
yudenghao <denghao1019@163.com>
2019-05-20 11:51:00 +0800

    add:修改上传视频

--- JiaMianWuHui/app/src/main/java/com/qsd/jmwh/module/home/user/presenter/AuthCenterPresenter.java
+++ JiaMianWuHui/app/src/main/java/com/qsd/jmwh/module/home/user/presenter/AuthCenterPresenter.java
@@ -16,32 +16,36 @@ import com.yu.common.loading.LoadingDialog;
  * @author yudneghao
  * @date 2019-05-17
  */
-@SuppressLint("CheckResult")
-public class AuthCenterPresenter extends BaseViewPresenter<AuthCenterViewer> {
+@SuppressLint("CheckResult") public class AuthCenterPresenter
+    extends BaseViewPresenter<AuthCenterViewer> {
 
   public AuthCenterPresenter(AuthCenterViewer viewer) {
     super(viewer);
   }
 
-   public void getAuthInf() {
-    XHttpProxy.proxy(OtherApiServices.class).getWomenVideo().subscribeWith(new TipRequestSubscriber<WomenVideoBean>() {
-      @Override protected void onSuccess(WomenVideoBean vedioBean) {
-        assert getViewer() != null;
-        if (vedioBean != null) {
-          getViewer().getInfo(vedioBean);
-        }
-
-      }
-    });
+  public void getAuthInf() {
+    XHttpProxy.proxy(OtherApiServices.class)
+        .getWomenVideo()
+        .subscribeWith(new TipRequestSubscriber<WomenVideoBean>() {
+          @Override protected void onSuccess(WomenVideoBean vedioBean) {
+            assert getViewer() != null;
+            if (vedioBean != null) {
+              getViewer().getInfo(vedioBean);
+            }
+          }
+        });
   }
 
   public void uploadAuthVideo(String path) {
-    LoadingDialog.showNormalLoading(getActivity(),false);
-    LoadingDialog.startLoading( "正在上传");
+    LoadingDialog.showNormalLoading(getActivity(), false);
+    LoadingDialog.startLoading("正在上传");
     new Thread(() -> {
-      PersistenceResponse response = UploadImage.uploadImage(getActivity(), UserProfile.getInstance().getObjectName("auth","mp4"), path);
-      XHttpProxy.proxy(OtherApiServices.class).userAuthByVideo(response.cloudUrl,"http://img0.imgtn.bdimg.com/it/u=1251096059,3284062985&fm=26&gp=0.jpg").subscribeWith(
-          new TipRequestSubscriber<Object>() {
+      PersistenceResponse response = UploadImage.uploadImage(getActivity(),
+          UserProfile.getInstance().getObjectName("auth", "mp4"), path);
+      String sFileCoverUrl = response.cloudUrl + "?x-oss-process=video/snapshot,t_100,m_fast";
+      XHttpProxy.proxy(OtherApiServices.class)
+          .userAuthByVideo(response.cloudUrl, sFileCoverUrl)
+          .subscribeWith(new TipRequestSubscriber<Object>() {
             @Override protected void onSuccess(Object o) {
               assert getViewer() != null;
               getViewer().uploadSuccess();
