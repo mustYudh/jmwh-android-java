24d92472c0f25e5ded9a2fbb2ada6d433b9493f3
yudenghao <denghao1019@163.com>
2019-06-24 16:28:42 +0800

    add:增加信息认证校验

--- JiaMianWuHui/app/src/main/java/com/qsd/jmwh/module/register/EditRegisterCodeActivity.java
+++ JiaMianWuHui/app/src/main/java/com/qsd/jmwh/module/register/EditRegisterCodeActivity.java
@@ -23,114 +23,110 @@ import com.yu.common.mvp.PresenterLifeCycle;
 import com.yu.common.toast.ToastUtils;
 import org.greenrobot.eventbus.EventBus;
 
-public class EditRegisterCodeActivity extends BaseBarActivity implements View.OnClickListener, EditRegisterCodeViewer {
-    @PresenterLifeCycle
-    EditRegisterCodePresenter mPresenter = new EditRegisterCodePresenter(this);
+public class EditRegisterCodeActivity extends BaseBarActivity
+    implements View.OnClickListener, EditRegisterCodeViewer {
+  @PresenterLifeCycle EditRegisterCodePresenter mPresenter = new EditRegisterCodePresenter(this);
 
-    @Override
-    protected void setView(@Nullable Bundle savedInstanceState) {
-        setContentView(R.layout.edit_register_code_activity_layout);
-    }
+  @Override protected void setView(@Nullable Bundle savedInstanceState) {
+    setContentView(R.layout.edit_register_code_activity_layout);
+  }
 
-    private static final String TOKEN = "token";
-    private static final String USER_ID = "user_id";
-    private static final String SEX = "sex";
-    private static final String TYPE = "sex";
+  private static final String TOKEN = "token";
+  private static final String USER_ID = "user_id";
+  private static final String SEX = "sex";
+  private static final String TYPE = "sex";
 
-    public static int GET_AUTH_CODE_REQUEST = 124;
-    public static String GET_AUTH_CODE_RESULT = "get_auth_code_result";
+  public static int GET_AUTH_CODE_REQUEST = 124;
+  public static String GET_AUTH_CODE_RESULT = "get_auth_code_result";
 
-    public static Intent getIntent(Context context, String token, int lUserId, int sex,int type) {
-        Intent starter = new Intent(context, EditRegisterCodeActivity.class);
-        starter.putExtra(TOKEN, token);
-        starter.putExtra(USER_ID, lUserId);
-        starter.putExtra(SEX, sex);
-        starter.putExtra(TYPE, type);
-        return starter;
-    }
+  public static Intent getIntent(Context context, String token, int lUserId, int sex, int type) {
+    Intent starter = new Intent(context, EditRegisterCodeActivity.class);
+    starter.putExtra(TOKEN, token);
+    starter.putExtra(USER_ID, lUserId);
+    starter.putExtra(SEX, sex);
+    starter.putExtra(TYPE, type);
+    return starter;
+  }
 
-    @Override
-    protected void loadData() {
-        setTitle("欢迎");
-        initListener();
-    }
+  @Override protected void loadData() {
+    setTitle("欢迎");
+    initListener();
+  }
 
-    private void initListener() {
-        bindView(R.id.to_register, this);
-        bindView(R.id.to_by, this);
-        bindView(R.id.login, this);
-    }
+  private void initListener() {
+    bindView(R.id.to_register, this);
+    bindView(R.id.to_by, this);
+    bindView(R.id.login, this);
+  }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.to_register:
-                getLaunchHelper().startActivityForResult(
-                        EditUserDataActivity.getIntent(getActivity(), getIntent().getStringExtra(TOKEN),
-                                getIntent().getIntExtra(USER_ID, -1), getIntent().getIntExtra(SEX, -1)), GET_AUTH_CODE_REQUEST);
-                break;
-            case R.id.to_by:
-                getLaunchHelper().startActivityForResult(ToByVipActivity.getIntent(getActivity(), getIntent().getIntExtra(USER_ID, -1), getIntent().getStringExtra(TOKEN)),GET_AUTH_CODE_REQUEST);
-                break;
-            case R.id.login:
-                EditText editText = bindView(R.id.code);
-                String code = editText.getText().toString().trim();
-                if (!TextUtils.isEmpty(code)) {
-                    SelectHintPop selectHintPop = new SelectHintPop(getActivity());
-                    selectHintPop.setTitle("验证码验证通过")
-                            .setMessage("欢迎加入假面舞会！请勿把您的的账户泄露给他人，一经发现登录异常，账户会被自动冻结。")
-                            .setSingleButton("好的", v1 -> {
-                                mPresenter.commitCode(getIntent().getIntExtra(USER_ID, -1), getIntent().getStringExtra(TOKEN), code);
-                                selectHintPop.dismiss();
-                            })
-                            .showPopupWindow();
-                } else {
-                    ToastUtils.show("邀请码输入不能为空");
-                }
-                break;
-            default:
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == Activity.RESULT_OK && data != null) {
-            String result = data.getStringExtra(GET_AUTH_CODE_RESULT);
-            Log.e("=========>获取成功",result);
-            EditText editText = bindView(R.id.code);
-            if (!TextUtils.isEmpty(result)) {
-                editText.setText(result);
-            }
-
-        }
-    }
-
-    @Override
-    public void registerSuccess() {
-        if (getIntent().getIntExtra(TYPE,-1) == 0) {
-            getLaunchHelper().startActivity(HomeActivity.class);
-        } else {
-            EventBus.getDefault().post(new RegisterSuccess(true));
-            LoginInfo info = new LoginInfo();
-            info.lUserId = getIntent().getIntExtra(USER_ID,-1);
-            info.token = getIntent().getStringExtra(TOKEN);
-            UserProfile.getInstance().appLogin(info);
-            getLaunchHelper().startActivity(HomeActivity.class);
-            finish();
-        }
-
-    }
-
-    @Override
-    public void getUserCode(String code) {
+  @Override public void onClick(View v) {
+    switch (v.getId()) {
+      case R.id.to_register:
+        getLaunchHelper().startActivityForResult(
+            EditUserDataActivity.getIntent(getActivity(), getIntent().getStringExtra(TOKEN),
+                getIntent().getIntExtra(USER_ID, -1), getIntent().getIntExtra(SEX, -1)),
+            GET_AUTH_CODE_REQUEST);
+        break;
+      case R.id.to_by:
+        getLaunchHelper().startActivityForResult(
+            ToByVipActivity.getIntent(getActivity(), getIntent().getIntExtra(USER_ID, -1),
+                getIntent().getStringExtra(TOKEN)), GET_AUTH_CODE_REQUEST);
+        break;
+      case R.id.login:
         EditText editText = bindView(R.id.code);
-        editText.setText(code);
+        String code = editText.getText().toString().trim();
+        if (!TextUtils.isEmpty(code)) {
+          SelectHintPop selectHintPop = new SelectHintPop(getActivity());
+          selectHintPop.setTitle("验证码验证通过")
+              .setMessage("欢迎加入假面舞会！请勿把您的的账户泄露给他人，一经发现登录异常，账户会被自动冻结。")
+              .setSingleButton("好的", v1 -> {
+                mPresenter.commitCode(getIntent().getIntExtra(USER_ID, -1),
+                    getIntent().getStringExtra(TOKEN), code);
+                selectHintPop.dismiss();
+              })
+              .showPopupWindow();
+        } else {
+          ToastUtils.show("邀请码输入不能为空");
+        }
+        break;
+      default:
     }
+  }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mPresenter.getUserCode(getIntent().getIntExtra(USER_ID,-1),getIntent().getStringExtra(TOKEN));
+  @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (resultCode == Activity.RESULT_OK && data != null) {
+      String result = data.getStringExtra(GET_AUTH_CODE_RESULT);
+      Log.e("=========>获取成功", result);
+      EditText editText = bindView(R.id.code);
+      if (!TextUtils.isEmpty(result)) {
+        editText.setText(result);
+        editText.setSelection(result.length());
+      }
     }
+  }
+
+  @Override public void registerSuccess() {
+    if (getIntent().getIntExtra(TYPE, -1) == 0) {
+      getLaunchHelper().startActivity(HomeActivity.class);
+    } else {
+      EventBus.getDefault().post(new RegisterSuccess(true));
+      LoginInfo info = new LoginInfo();
+      info.lUserId = getIntent().getIntExtra(USER_ID, -1);
+      info.token = getIntent().getStringExtra(TOKEN);
+      UserProfile.getInstance().appLogin(info);
+      getLaunchHelper().startActivity(HomeActivity.class);
+      finish();
+    }
+  }
+
+  @Override public void getUserCode(String code) {
+    EditText editText = bindView(R.id.code);
+    editText.setText(code);
+  }
+
+  @Override protected void onResume() {
+    super.onResume();
+    mPresenter.getUserCode(getIntent().getIntExtra(USER_ID, -1), getIntent().getStringExtra(TOKEN));
+  }
 }
--- JiaMianWuHui/app/src/main/java/com/qsd/jmwh/module/register/bean/UploadUserInfoParams.java
+++ JiaMianWuHui/app/src/main/java/com/qsd/jmwh/module/register/bean/UploadUserInfoParams.java
@@ -1,57 +1,61 @@
 package com.qsd.jmwh.module.register.bean;
 
+import android.text.TextUtils;
+import com.yu.common.toast.ToastUtils;
+
 public class UploadUserInfoParams {
-    public String sNickName = "";
-    public String sDateRange = "";
-    public String sAge = "";
-    public String sJob = "";
-    public String sDatePro = "";
-    public String sHeight = "";
-    public String sWeight = "";
-    public String lUserId = "";
-    public String sIntroduce = "";
-    public String token = "";
-    public String sBust = "";
-    public String sUserHeadPic = "";
-    public String QQ = "";
-    public String WX = "";
-    public boolean bHiddenQQandWX = true;
-//
-//    public boolean checkEmpty() {
-//        if (!TextUtils.isEmpty(sNickName)) {
-//            ToastUtils.show("昵称不能为空");
-//            return false;
-//        }
-//        if (!TextUtils.isEmpty(sDateRange)) {
-//            ToastUtils.show("约会范围不能为空");
-//            return false;
-//        }
-//        if (!TextUtils.isEmpty(sAge)) {
-//            ToastUtils.show("年龄不能为空");
-//            return false;
-//        }
-//        if (!TextUtils.isEmpty(sJob)) {
-//            ToastUtils.show("职业不能为空");
-//            return false;
-//        }
-//        if (!TextUtils.isEmpty(sDatePro)) {
-//            ToastUtils.show("约会节目不能为空");
-//            return false;
-//        }
-//        if (!TextUtils.isEmpty(sHeight)) {
-//            ToastUtils.show("身高不能为空");
-//            return false;
-//        }
-//        if (!TextUtils.isEmpty(sWeight)) {
-//            ToastUtils.show("体重不能为空");
-//            return false;
-//        }
-//        if (!TextUtils.isEmpty(sUserHeadPic)) {
-//            ToastUtils.show("请设置头像");
-//            return false;
-//        }
-//        return true;
-//    }
+  public String sNickName = "";
+  public String sDateRange = "";
+  public String sAge = "";
+  public String sJob = "";
+  public String sDatePro = "";
+  public String sHeight = "";
+  public String sWeight = "";
+  public String lUserId = "";
+  public String sIntroduce = "";
+  public String token = "";
+  public String sBust = "";
+  public String sUserHeadPic = "";
+  public String QQ = "";
+  public String WX = "";
+  public boolean bHiddenQQandWX = true;
 
-
+  public boolean checkEmpty(boolean isGirl) {
+    if (TextUtils.isEmpty(sUserHeadPic)) {
+      ToastUtils.show("请设置头像");
+      return false;
+    } else if (TextUtils.isEmpty(sNickName)) {
+      ToastUtils.show("昵称不能为空");
+      return false;
+    } else if (TextUtils.isEmpty(sDateRange)) {
+      ToastUtils.show("约会范围不能为空");
+      return false;
+    } else if (TextUtils.isEmpty(sAge)) {
+      ToastUtils.show("年龄不能为空");
+      return false;
+    } else if (TextUtils.isEmpty(sJob)) {
+      ToastUtils.show("职业不能为空");
+      return false;
+    } else if (TextUtils.isEmpty(sDatePro)) {
+      ToastUtils.show("约会节目不能为空");
+      return false;
+    } else if (TextUtils.isEmpty(QQ) && TextUtils.isEmpty(WX)) {
+      ToastUtils.show("微信或者QQ必须填写一项");
+      return false;
+    } else if (TextUtils.isEmpty(sHeight)) {
+      ToastUtils.show("身高不能为空");
+      return false;
+    } else if (TextUtils.isEmpty(sWeight)) {
+      ToastUtils.show("体重不能为空");
+      return false;
+    } else if (isGirl && TextUtils.isEmpty(sBust)) {
+      ToastUtils.show("胸围不能为空");
+      return false;
+    } else if (TextUtils.isEmpty(sIntroduce)) {
+      ToastUtils.show("个人介绍不能为空");
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
