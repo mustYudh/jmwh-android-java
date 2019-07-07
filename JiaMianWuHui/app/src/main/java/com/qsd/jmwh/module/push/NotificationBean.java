592cbf4dbdad0c289c247d87e15fe967387d2971
yudenghao <denghao.yu@yuntu-inc.com>
2019-07-07 23:20:31 +0800

    add：加入推送

---
+++ JiaMianWuHui/app/src/main/java/com/qsd/jmwh/module/push/NotificationBean.java
@@ -0,0 +1,13 @@
+package com.qsd.jmwh.module.push;
+
+import java.io.Serializable;
+
+
+public class NotificationBean implements Serializable {
+
+  public String title;
+  public String message;
+  public String content;
+
+
+}
