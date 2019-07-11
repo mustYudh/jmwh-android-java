8b5260a1ceb8d59c6e1234f380f50a08813e2a23
yudenghao <denghao1019@163.com>
2019-07-11 11:00:37 +0800

    add:修复token异常

--- JiaMianWuHui/app/src/main/java/com/qsd/jmwh/module/splash/SplashActivity.java
+++ JiaMianWuHui/app/src/main/java/com/qsd/jmwh/module/splash/SplashActivity.java
@@ -11,9 +11,15 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
+import com.netease.nim.uikit.api.NimUIKit;
+import com.netease.nim.uikit.common.ToastHelper;
+import com.netease.nimlib.sdk.NIMClient;
+import com.netease.nimlib.sdk.RequestCallback;
+import com.netease.nimlib.sdk.auth.AuthService;
 import com.qsd.jmwh.R;
 import com.qsd.jmwh.base.BaseActivity;
 import com.qsd.jmwh.data.UserProfile;
+import com.qsd.jmwh.dialog.net.NetLoadingDialog;
 import com.qsd.jmwh.module.home.HomeActivity;
 import com.qsd.jmwh.module.login.LoginActivity;
 import com.qsd.jmwh.module.login.bean.LoginInfo;
@@ -47,12 +53,12 @@ public class SplashActivity extends BaseActivity
         Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION,
         Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE,
         Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_PHONE_STATE,
-        Manifest.permission.CALL_PHONE,
-        Manifest.permission.RECORD_AUDIO,
+        Manifest.permission.CALL_PHONE, Manifest.permission.RECORD_AUDIO,
     };
     if (Build.VERSION.SDK_INT >= 23) {
       final RxPermissions rxPermissions = new RxPermissions(this);
-      rxPermissions.request(permiss).subscribe(granted -> {});
+      rxPermissions.request(permiss).subscribe(granted -> {
+      });
     }
     if (UserProfile.getInstance().isAppLogin()) {
       getLaunchHelper().startActivityForResult(HomeActivity.class, REQUEST_CODE);
@@ -146,9 +152,38 @@ public class SplashActivity extends BaseActivity
   @Override public void authLoginSuccess(LoginInfo loginInfo) {
     if (loginInfo != null) {
       UserProfile.getInstance().appLogin(loginInfo);
-      getLaunchHelper().startActivity(HomeActivity.class);
-      setResult(Activity.RESULT_OK);
-      finish();
+      NetLoadingDialog.showLoading(getActivity(), false);
+      NIMClient.getService(AuthService.class)
+          .login(new com.netease.nimlib.sdk.auth.LoginInfo(loginInfo.sIMID, loginInfo.sIMToken))
+          .setCallback(new RequestCallback<com.netease.nimlib.sdk.auth.LoginInfo>() {
+
+            @Override public void onSuccess(com.netease.nimlib.sdk.auth.LoginInfo info) {
+              UserProfile.getInstance().appLogin(loginInfo);
+              UserProfile.getInstance().setSex(loginInfo.nSex);
+              NimUIKit.loginSuccess(loginInfo.sIMID);
+              NIMClient.toggleNotification(true);
+              EventBus.getDefault().post(new RegisterSuccess(true));
+              NetLoadingDialog.dismissLoading();
+              getLaunchHelper().startActivity(HomeActivity.class);
+              setResult(Activity.RESULT_OK);
+              finish();
+            }
+
+            @Override public void onFailed(int code) {
+              if (code == 302 || code == 404) {
+                ToastHelper.showToast(SplashActivity.this, "帐号或密码错误");
+                NetLoadingDialog.dismissLoading();
+              } else {
+                ToastHelper.showToast(SplashActivity.this, "登录失败: " + code);
+                NetLoadingDialog.dismissLoading();
+              }
+            }
+
+            @Override public void onException(Throwable throwable) {
+              ToastHelper.showToast(SplashActivity.this, "无效输入");
+              NetLoadingDialog.dismissLoading();
+            }
+          });
     }
   }
 
